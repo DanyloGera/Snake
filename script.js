13fd96a2a05d2b9353d7e1bf4ec2778327e1e78e
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-10 13:08:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function drawGame() {
 
   }
   if (score === 20) {
-    clearInterval(game, alert("You did it!"), document.location.href="https://danylogera.github.io/game-5/");
+    clearInterval(game, alert("You did it!Your score: +"  + score), document.location.href="https://danylogera.github.io/game-5/");
   }
 
 //межі поля
